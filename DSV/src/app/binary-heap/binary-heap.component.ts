65e4e6c473042756b31775106e03fca4ef5a0cb1
AlexePaul <64796131+AlexePaul@users.noreply.github.com>
2023-11-01 20:14:48 +0200

    Added Heap Animations
    To do:
     -> Heap still needs a way to change between min and max heap
     -> implement heapify to not erase the heap when changing mode

--- DSV/src/app/binary-heap/binary-heap.component.ts
+++ DSV/src/app/binary-heap/binary-heap.component.ts
@@ -10,9 +10,12 @@ export class BinaryHeapComponent {
   value : number = 0;
   bHeap: binaryHeap;
   tooMuch: number = 0;
+  animation: boolean = false;
+  stopAnimation: boolean = false;
 
   constructor(){
-    this.bHeap = new binaryHeap();
+    this.draw = this.draw.bind(this);
+    this.bHeap = new binaryHeap(true, this.draw);
   }
 
   drawLines(circleRadius : any){
@@ -75,49 +78,103 @@ export class BinaryHeapComponent {
 
   }
 
-  draw(){
+  async delay(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async fadingCircle(x:any, y:any, circleRadius:any, startingAngle:any, endAngle:any, startingColor:string = "Black"){
+    this.animation = true;
+    const canvas = document.getElementById("Canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    if(ctx == null)
+      return;
+    let value = 255;
+    while(value > 64){
+      if(this.stopAnimation == true)
+        return;
+      ctx.beginPath();
+      ctx.arc(x, y, circleRadius, startingAngle, endAngle);
+      if(startingColor == "Red")
+      ctx.strokeStyle = "rgb("+value+",0,0)";
+      else if(startingColor == "Blue")
+      ctx.strokeStyle = "rgb(0,0,"+value+")";
+      else
+      ctx.strokeStyle = "rgb(0,"+value+",0)";
+        value -= 2;
+      ctx.stroke();
+      await this.delay(1)
+    }
+    this.animation = false;
+  }
+
+  async draw(specialNumbers:number[], specialColor: string){
+    
+    this.stopAnimation = true;
+    await this.delay(10);
+
+    this.stopAnimation = false;
     const canvas = document.getElementById("Canvas") as HTMLCanvasElement;
     const ctx = canvas.getContext('2d');
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-    
-    let numberOfNodes = 1;
-    let size = this.bHeap.size;
-    let step = 0;
-    let counter = 1;
-    let circleRadius = 32 * windowWidth / 1080;
+    var nrs = specialNumbers;
 
+    var numberOfNodes = 1;
+    var size = this.bHeap.size;  
+    var step = 0;
+    var counter = 1;
+    var circleRadius = 32 * windowWidth / 1080;
+    
     if(ctx == null)
-      return;
-    ctx.canvas.width  = windowWidth;
-    ctx.canvas.height = windowHeight;
+    return new Promise(resolve => setTimeout(resolve, 1));
     
+      ctx.canvas.width  = windowWidth;
+    ctx.canvas.height = windowHeight;
     while(size > 0){
-      let space = Math.floor(windowWidth / (numberOfNodes*2));
+      var space = Math.floor(windowWidth / (numberOfNodes*2));
       for(var i = 0; i < Math.min(numberOfNodes,size); i++){
-        ctx.beginPath();
-        if(i == 0)
-          ctx.arc(space, 150*(step+1), circleRadius, 0, 2* Math.PI);
-        else
-          ctx.arc(space + 2*space*i, 150*(step+1), circleRadius, 0, 2* Math.PI);
-        ctx.stroke();
+        if(nrs.indexOf(Number(counter)) == -1){
+          ctx.beginPath();
+          if(i == 0)
+            ctx.arc(space, 150*(step+1), circleRadius, 0, 2* Math.PI);
+          else
+            ctx.arc(space + 2*space*i, 150*(step+1), circleRadius, 0, 2* Math.PI);
+          ctx.stroke();
 
-        if(i==0){
-          ctx.font = "16px Tahoma";
-          ctx.fillText(String(this.bHeap.array[counter++]),space,150*(step+1));
+          if(i==0){
+            ctx.font = "16px Tahoma";
+            ctx.fillText(String(this.bHeap.array[counter++]),space,150*(step+1));
+          }
+          else{
+            ctx.font = "16px Tahoma";
+            ctx.fillText(String(this.bHeap.array[counter++]),space + 2*space*i,150*(step+1));
+          }
         }
         else{
-          ctx.font = "16px Tahoma";
-          ctx.fillText(String(this.bHeap.array[counter++]),space + 2*space*i,150*(step+1));
+          if(i == 0)
+            this.fadingCircle(space, 150*(step+1), circleRadius, 0, 2* Math.PI, specialColor);
+          else
+            this.fadingCircle(space + 2*space*i, 150*(step+1), circleRadius, 0, 2* Math.PI, specialColor);
+          ctx.strokeStyle="Black"
+          if(i==0){
+            ctx.font = "16px Tahoma";
+            ctx.fillText(String(this.bHeap.array[counter++]),space,150*(step+1));
+          }
+          else{
+            ctx.font = "16px Tahoma";
+            ctx.fillText(String(this.bHeap.array[counter++]),space + 2*space*i,150*(step+1));
+          }
         }
       }
-      step++;
-      size -= numberOfNodes;
-      numberOfNodes *= 2;
+        step++;
+        size -= numberOfNodes;
+        numberOfNodes *= 2;
     }
-
     this.drawLines(circleRadius);
-
+    while(this.animation == true){
+      await this.delay(1);
+    }
+    return new Promise(resolve => setTimeout(resolve, 1));
   }
 
   only1binary(value : number) : boolean{
@@ -129,12 +186,12 @@ export class BinaryHeapComponent {
     return true;
   }
 
-  insert(){
+  async insert(){
     let maxAmount = Math.max(31 * window.innerWidth / 1920,31 * window.innerHeight / 1080);
     while(this.only1binary(maxAmount) == false)
       maxAmount--;
     if(this.bHeap.size < maxAmount){
-      this.bHeap.insertValue(this.value);
+      await this.bHeap.insertValue(this.value);
       if(this.bHeap.size == maxAmount)
         this.tooMuch = 0.5;
     }
@@ -142,9 +199,11 @@ export class BinaryHeapComponent {
       this.tooMuch = 0.5;
     else
       this.tooMuch = 1;
+    this.draw([], "");
   }
-  remove(){
-    this.bHeap.removeRoot();
+  async remove(){
+    await this.bHeap.removeRoot();
     this.tooMuch = 0;
+    this.draw([], "");
   }
 }
--- DSV/src/shared/models/binaryHeap.ts
+++ DSV/src/shared/models/binaryHeap.ts
@@ -2,15 +2,24 @@ export class binaryHeap{
     array: number[] = [];
     minimum: boolean;
     size : number = 0;
-    constructor(minimum : boolean = true){
+    draw: (specialNumbers: number[], specialColor:string) => void
+
+    constructor(minimum : boolean = true, draw:any){
         this.minimum = minimum;
+        this.draw = draw;
     }
 
-    insertValue(value: number){
+    async delay(ms:number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+    async insertValue(value: number){
         // Insert the new value at the end of a the array (If the heap structure is broken, we will need to repair it)
         this.size++;
         this.array[this.size] =  value;
 
+        await this.draw([this.size], "Blue");
+        
         //will use this in order to make it work for both a min heap and a max heap
         var term = 1;
         if(this.minimum == false)
@@ -22,16 +31,21 @@ export class binaryHeap{
         while(position != 1 && this.array[Math.floor(position/2)]*term > this.array[position]*term){
             // if the position i'm on is smaller than it's father, i need to swap them
             [this.array[Math.floor(position/2)], this.array[position]] = [this.array[position], this.array[Math.floor(position/2)]]; 
+            await this.draw([position, Math.floor(position/2)], "Green");
             position = Math.floor(position/2);
         }
     }
 
-    removeRoot(){
+    async removeRoot(){
         var rootValue = this.array[1];
+
         [this.array[1], this.array[this.size]] = [this.array[this.size],this.array[1]];
+        await this.draw([1,this.size], "Green");
+        await this.delay(10);
+        await this.draw([this.size], "Red");
+        await this.delay(10);
         this.size--;
         let position = 1;
-
         var term = 1;
         if(this.minimum == false)
             term = -1;
@@ -41,11 +55,13 @@ export class binaryHeap{
                 if(this.array[position]*term > Math.min(this.array[position*2]*term, this.array[position*2+1]*term)){
                     if(this.array[position*2]*term < this.array[position*2+1]*term){
                         [this.array[position],this.array[position*2]] = [this.array[position*2], this.array[position]];
+                        await this.draw([position,position*2], "Green");
                         position *= 2; 
                         continue;
                     }   
                     else{
                         [this.array[position],this.array[position*2 + 1]] = [this.array[position*2 + 1], this.array[position]];
+                        await this.draw([position,position*2 + 1], "Green");
                         position = position * 2 + 1; 
                         continue;
                     }
