795692f1d001bea1a5abe90acf7e822524cc38b3
AlexePaul <64796131+AlexePaul@users.noreply.github.com>
2023-11-15 22:26:28 +0200

    Implemented animations in HashTable
    Also implemented:
    ->removeValue which is used to remove a value from the table
    ->searchValue which is used to search for a value inside the table

--- DSV/src/app/hash-table/hash-table.component.ts
+++ DSV/src/app/hash-table/hash-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, Renderer2 } from '@angular/core';
 import { hashTable } from 'src/shared/models/hashTable';
 
 @Component({
@@ -7,19 +7,28 @@ import { hashTable } from 'src/shared/models/hashTable';
   styleUrls: ['./hash-table.component.less']
 })
 export class HashTableComponent {
-  valueInsert: number = 0;
+  value: number = 0;
   disabled: boolean = false;
   hashTable: hashTable;
   modulo: number = 10;
-  
+  animation : boolean = false;
+
   hashFunction(value:number) : number{
     return value%this.modulo;
   }
 
-  constructor(){
+  constructor(private cdr: ChangeDetectorRef){
     this.draw = this.draw.bind(this);
     this.hashFunction = this.hashFunction.bind(this);
-    this.hashTable = new hashTable(this.hashFunction,this.modulo);
+    this.hashTable = new hashTable(this.hashFunction,this.modulo, this.draw);
+  }
+  
+  ngOnInit() {
+    this.draw(-1, [], "");
+  }
+
+  async delay(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   drawRectangle(x : any, y : any, width : any, height : any, color : string = "black"){
@@ -38,7 +47,65 @@ export class HashTableComponent {
     }
   }
 
-  async draw(specialNumbers:number[], specialColor: string){
+  async fadingRectangle(x : any, y : any, width : any, height : any, startingColor:string = "Black", color:string = ""){
+    this.animation = true;
+    const canvas = document.getElementById("Canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    if(ctx == null)
+      return;
+    let value = 255;
+    while(value > 128){
+      ctx.strokeRect(x, y, width, height);
+      if(color == "rgb(255,255,255)"){
+        if(startingColor == "Red")
+          ctx.strokeStyle = "rgb("+255+","+(255-value)+","+(255-value)+")";
+        else if(startingColor == "Blue")
+          ctx.strokeStyle = "rgb("+(255-value)+","+(255-value)+","+255+")";
+        else
+          ctx.strokeStyle = "rgb("+(255-value)+","+255+","+(255-value)+")";
+      }
+      else{
+        if(startingColor == "Red")
+          ctx.strokeStyle = "rgb("+value+",0,0)";
+        else if(startingColor == "Blue")
+          ctx.strokeStyle = "rgb(0,0,"+value+")";
+        else
+          ctx.strokeStyle = "rgb(0,"+value+",0)";
+      }
+        value -= 1;
+      await this.delay(10)
+    }
+    this.animation = false;
+  }
+
+  checkSpecial(special:number[][], i:any, j:any) :number{
+    let ans = -1;
+
+    for(let nr of special){
+      if(nr[0] == i && nr[1] == j)
+        return 1;
+    }
+
+    return ans;
+  }
+
+  async checkModulo(){
+
+    while(this.animation == true){
+      await this.delay(1);
+    }
+
+    let maxModulo = Math.floor(window.innerHeight * 15 / 919);
+    this.modulo = Math.max(this.modulo, 1);
+    this.modulo = Math.min(this.modulo, maxModulo);
+
+    this.hashTable.reset(this.hashFunction, this.modulo);
+    console.log(this.hashTable.array);
+    this.cdr.detectChanges();
+    this.draw(-1, [], "");
+  }
+
+  async draw(specialCategoryNumber:number, specialNumbers:number[][], specialColor: string){
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     var myColor = "rgb(0,0,0)"
     if(prefersDarkMode)
@@ -61,7 +128,10 @@ export class HashTableComponent {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = myColor;
     for(let i = 0; i < this.modulo; i++){
-      this.drawRectangle(windowWidth*0.05,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, myColor);
+      if(specialCategoryNumber!=i)
+        this.drawRectangle(windowWidth*0.05,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, myColor);
+      else
+        this.drawRectangle(windowWidth*0.05,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, "rgb(0,255,0)");  
       ctx.beginPath();
       ctx.moveTo(windowWidth*0.05 + 3 * rectangleWidth/4,(rectangleHeight + 10)*(i+1));
       ctx.lineTo(windowWidth*0.05 + 3 * rectangleWidth/4,(rectangleHeight + 10)*(i+1) + rectangleHeight);
@@ -75,8 +145,12 @@ export class HashTableComponent {
 
     for(let i = 0; i < this.modulo; i++){
       let idx = 1;
-      for(let item of this.hashTable.array[i]){
-        this.drawRectangle(windowWidth*0.05 + (rectangleWidth + 25) * idx,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, myColor);
+      for(let j = 0; j < this.hashTable.array[i]?.length; j++){
+        if(this.checkSpecial(specialNumbers, i, j) == -1)
+          this.drawRectangle(windowWidth*0.05 + (rectangleWidth + 25) * idx,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, myColor);
+        else
+          this.fadingRectangle(windowWidth*0.05 + (rectangleWidth + 25) * idx,(rectangleHeight + 10)*(i+1), rectangleWidth, rectangleHeight, specialColor, myColor);
+        ctx.strokeStyle = myColor;
         ctx.beginPath();
         ctx.moveTo(windowWidth*0.05 + (rectangleWidth + 25) * idx + 3 * rectangleWidth/4,(rectangleHeight + 10)*(i+1));
         ctx.lineTo(windowWidth*0.05 + (rectangleWidth + 25) * idx + 3 * rectangleWidth/4,(rectangleHeight + 10)*(i+1) + rectangleHeight);
@@ -85,7 +159,7 @@ export class HashTableComponent {
         ctx.textBaseline = "middle";
         ctx.font = rectangleHeight/3+"px Tahoma";
         ctx.fillStyle=myColor;
-        ctx.fillText(String(item),  windowWidth*0.05 + (rectangleWidth + 25) * idx + 3 * rectangleWidth/8,(rectangleHeight + 10)*(i+1) + rectangleHeight/2);
+        ctx.fillText(String(this.hashTable.array[i][j]),  windowWidth*0.05 + (rectangleWidth + 25) * idx + 3 * rectangleWidth/8,(rectangleHeight + 10)*(i+1) + rectangleHeight/2);
 
         ctx.beginPath();
         ctx.moveTo(windowWidth*0.05 + (rectangleWidth + 25) * (idx-1) + 7 * rectangleWidth/8,(rectangleHeight + 10)*(i+1) + rectangleHeight/2);
@@ -100,11 +174,30 @@ export class HashTableComponent {
         idx++;
       }
     }
-
+    while(this.animation == true){
+      await this.delay(1);
+    }
+    return new Promise(resolve => setTimeout(resolve, 10));
   }
 
-  insert(){
-    this.hashTable.insertValue(this.valueInsert);
-    this.draw([], "");
+  async insert(){
+    this.disabled = true;
+    await this.hashTable.insertValue(this.value);
+    this.draw(-1, [], "");
+    this.disabled = false;
+  }
+
+  async remove(){
+    this.disabled = true;
+    await this.hashTable.removeValue(this.value)
+    this.draw(-1, [],"");
+    this.disabled = false;
+  }
+
+  async search(){
+    this.disabled = true;
+    await this.hashTable.searchValue(this.value)
+    this.draw(-1, [],"");
+    this.disabled = false;
   }
 }
