556e55d5245c54456e4f3595f8f33532d6c3ed80
AlexePaul <64796131+AlexePaul@users.noreply.github.com>
2024-05-31 14:00:44 +0300

    Small bug fix - SLL next row arrow

--- DSV/src/shared/models/nodeSLL.ts
+++
@@ -1,9 +0,0 @@
-export class nodeSLL{
-    value : number = 0;
-    next : nodeSLL | null = null;
-    new : boolean = true; //this is used for animations
-    constructor(value:number, next:nodeSLL|null = null){
-        this.value = value;
-        this.next = next;
-    }
-};
